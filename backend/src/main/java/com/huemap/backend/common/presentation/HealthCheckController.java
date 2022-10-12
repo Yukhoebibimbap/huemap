373e8a7eab38264b31b16de7a369bf22e2d50337
wltks98 <wltks98@hanmail.net>
2022-10-12 13:49:33 +0900

    [#12] chore: 도커 설정파일 생성 & healthcheck 컨트롤러 생성

---
+++ backend/src/main/java/com/huemap/backend/common/presentation/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.huemap.backend.common.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthCheck")
+public class HealthCheckController {
+
+    @GetMapping
+    public String healthCheck(){
+
+
+        return "휴맵 스프링 서버";
+    }
+}
