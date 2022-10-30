6ec98ab116543462a9805b8a90a88fdce4160bfa
K4N9 <rkdalstjd21001@gmail.com>
2022-10-30 23:34:27 +0900

    [#59] feat: 지도 페이지 작성

---
+++ frontend/lib/map-page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:huemap_app/get_current_position.dart';
+import 'package:huemap_app/bin.dart';
+
+void main(){runApp(MaterialApp(home:MapPage()));}
+
+
+class MapPage extends StatefulWidget {
+  final String url = "118.67.130.12/map.html";
+
+  // 채널을 설정, 쓰레기통 정보 가져오고 객체화
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  // 웹뷰와의 통신을 위한 컨트롤러, 채널
+  WebViewController? controller;
+  Set<JavascriptChannel>? channel;
+
+  List<Bin>? bins;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Container(
+          height: 480,
+          child: WebView(
+            initialUrl: widget.url,
+            onWebViewCreated: (controller) {
+              this.controller = controller;
+              bins = [Bin(controller, 33.450701, 126.570667, 0)];
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            javascriptChannels: channel,
+          ),
+        ),
+      ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    channel = {JavascriptChannel(
+        name: 'onClickMarker', onMessageReceived: (message) {
+      Fluttertoast.showToast(msg: message.message);
+    })};
+  }
+
+  void setCategoryMarkers(int category)
+  {
+
+  }
+
+  void unsetCategoryMarkers(int category)
+  {
+
+  }
+}
