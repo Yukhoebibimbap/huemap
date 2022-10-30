2c67383ee00cb4544a401fdfdbb9cde96976dc97
wltks98 <wltks98@hanmail.net>
2022-10-30 09:59:16 +0900

    [#40] feat: url 수정, binService 추가

--- backend/src/main/java/com/huemap/backend/bin/presentation/BinController.java
+++ backend/src/main/java/com/huemap/backend/bin/presentation/BinController.java
@@ -5,19 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.huemap.backend.bin.application.BinService;
 import com.huemap.backend.common.response.success.RestResponse;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/bins")
+@RequestMapping("api/v1/bins")
 @RequiredArgsConstructor
 public class BinController {
+
+	private final BinService binService;
 
 	@GetMapping
 	public ResponseEntity<RestResponse> findAll() {
 
-		RestResponse response = RestResponse.of(null);
+		RestResponse response = RestResponse.of(binService.findAll());
 
 		return ResponseEntity.ok(response);
 	}
