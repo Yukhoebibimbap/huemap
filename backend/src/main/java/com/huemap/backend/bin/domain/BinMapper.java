a5118acd8c7ecf121c69b0453c692202e61c9d49
wltks98 <wltks98@hanmail.net>
2022-10-30 20:03:05 +0900

    [#40] feat: BinMapper 생성

---
+++ backend/src/main/java/com/huemap/backend/bin/domain/BinMapper.java
@@ -0,0 +1,19 @@
+package com.huemap.backend.bin.domain;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import com.huemap.backend.bin.dto.response.BinResponse;
+
+@Mapper(
+	componentModel = "spring",
+	injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+	unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public interface BinMapper {
+	BinMapper INSTANCE = Mappers.getMapper(BinMapper.class);
+
+	BinResponse ToDto(Bin bin);
+}
