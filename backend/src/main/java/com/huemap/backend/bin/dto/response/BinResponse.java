1e0a2f2471ec1f6cc9eaa55516359b089c0de229
wltks98 <wltks98@hanmail.net>
2022-10-30 09:59:40 +0900

    [#40] feat: Bin response dto 생성

---
+++ backend/src/main/java/com/huemap/backend/bin/dto/response/BinResponse.java
@@ -0,0 +1,19 @@
+package com.huemap.backend.bin.dto.response;
+
+import com.huemap.backend.bin.domain.BinType;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class BinResponse {
+
+	private Long id;
+	private String gu;
+	private Double latitude;
+	private Double longitude;
+	private String address;
+	private String addressDescription;
+	private BinType binType;
+}
---
+++ backend/src/main/java/com/huemap/backend/bin/dto/response/BinResponses.java
@@ -0,0 +1,13 @@
+package com.huemap.backend.bin.dto.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class BinResponses {
+	private List<BinResponse> binResponses;
+
+}
