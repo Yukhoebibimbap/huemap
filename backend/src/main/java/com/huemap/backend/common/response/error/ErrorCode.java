655e94544e2ffd43762e3c123155f9cc0d18fae8
wltks98 <wltks98@hanmail.net>
2022-11-02 08:53:51 +0900

    [#76] style : BIN_FOT_FOUND 메세지 변경

--- backend/src/main/java/com/huemap/backend/common/response/error/ErrorCode.java
+++ backend/src/main/java/com/huemap/backend/common/response/error/ErrorCode.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ErrorCode {
 	INVALID_INPUT_VALUE(400, "적절하지 않은 요청 값입니다."),
 	INTERNAL_SERVER_ERROR(500, "서버 내부에 오류가 생겼습니다."),
-	BIN_NOT_FOUND(400, "폐수거함이 존재하지 않습니다.");
+	BIN_NOT_FOUND(400, "폐수거함을 찾을 수 없습니다.");
 
 	private final int status;
 	private final String message;
