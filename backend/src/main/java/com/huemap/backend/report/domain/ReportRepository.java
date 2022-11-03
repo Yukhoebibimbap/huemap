59b3861fc378a4f2d11b4e224c899ee09285bc07
wltks98 <wltks98@hanmail.net>
2022-11-03 13:36:22 +0900

    [#87] feat : 폐수거함별 clouser 제보 갯수 세는 쿼리 추가

--- backend/src/main/java/com/huemap/backend/report/domain/ReportRepository.java
+++ backend/src/main/java/com/huemap/backend/report/domain/ReportRepository.java
@@ -3,10 +3,15 @@ package com.huemap.backend.report.domain;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.huemap.backend.bin.domain.Bin;
 
 public interface ReportRepository<T extends Report> extends JpaRepository<T, Long> {
 
-  Optional<Closure> findByUserIdAndBin(Long userId, Bin bin);
+	Optional<Closure> findByUserIdAndBin(Long userId, Bin bin);
+
+	@Query("select count(c) from Closure c where c.bin.id = :binId")
+	int countClosureByBin(@Param("binId") Long binId);
 }
