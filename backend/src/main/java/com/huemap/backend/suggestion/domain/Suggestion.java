32c09a05ba82ec9094fa78c4c3ece7e56187d4b4
wltks98 <wltks98@hanmail.net>
2022-11-04 16:44:03 +0900

    [#97] feat : Suggestion 엔티티 구현

---
+++ backend/src/main/java/com/huemap/backend/suggestion/domain/Suggestion.java
@@ -0,0 +1,36 @@
+package com.huemap.backend.suggestion.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.locationtech.jts.geom.Point;
+
+import com.huemap.backend.bin.domain.BinType;
+import com.huemap.backend.common.entity.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table
+public class Suggestion extends BaseEntity {
+
+	@NotNull
+	private String gu;
+
+	@Column(columnDefinition = "GEOMETRY")
+	private Point location;
+
+	@Enumerated(EnumType.STRING)
+	private BinType type;
+
+	@NotNull
+	private Long userId;
+}
