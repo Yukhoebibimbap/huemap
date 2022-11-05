7be31d559cc3f7441267fabc6a26605f58333acc
ecvheo1 <ecvheo@naver.com>
2022-11-05 20:00:24 +0900

    [#99] feat: 후보 폐수거함 생성 구현

--- backend/src/main/java/com/huemap/backend/bin/application/BinService.java
+++ backend/src/main/java/com/huemap/backend/bin/application/BinService.java
@@ -3,6 +3,7 @@ package com.huemap.backend.bin.application;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +14,12 @@ import com.huemap.backend.bin.domain.mapper.BinDetailMapper;
 import com.huemap.backend.bin.domain.mapper.BinMapper;
 import com.huemap.backend.bin.dto.response.BinDetailResponse;
 import com.huemap.backend.bin.dto.response.BinResponse;
+import com.huemap.backend.bin.event.BinCreateEvent;
 import com.huemap.backend.common.exception.EntityNotFoundException;
+import com.huemap.backend.common.exception.InvalidValueException;
 import com.huemap.backend.common.response.error.ErrorCode;
+import com.huemap.backend.openApi.kakao.KakaoMapProvider;
+import com.huemap.backend.openApi.kakao.response.KakaoMapRoadAddress;
 import com.huemap.backend.report.domain.ReportRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -26,6 +31,7 @@ public class BinService {
 
 	private final BinRepository binRepository;
 	private final ReportRepository reportRepository;
+	private final KakaoMapProvider kakaoMapProvider;
 
 	public List<BinResponse> findAll(final BinType type) {
 
@@ -43,5 +49,23 @@ public class BinService {
 		int count = reportRepository.countClosureByBin(id);
 
 		return BinDetailMapper.INSTANCE.toDto(bin, count != 0 ? true : false);
+	}
+
+	public void save(BinCreateEvent binCreateEvent) {
+		validateCandidateBinAlreadyExist(binCreateEvent.getType(), binCreateEvent.getLocation());
+
+		final KakaoMapRoadAddress addressInformation = kakaoMapProvider.getAddressInformation(
+				binCreateEvent.getLocation().getY(), binCreateEvent.getLocation().getX());
+
+		binRepository.save(Bin.candidateOf(binCreateEvent.getLocation(),
+																			 binCreateEvent.getType(),
+																			 addressInformation));
+	}
+
+	private void validateCandidateBinAlreadyExist(BinType type, Point location) {
+		binRepository.findCandidateBinByTypeAndLocation(type, location)
+								 .ifPresent(b -> {
+									 throw new InvalidValueException(ErrorCode.CANDIDATE_BIN_DUPLICATED);
+								 });
 	}
 }
