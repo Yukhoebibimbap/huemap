2e4b02d9d4c6237699ac8609f5a41ad33468a9a6
wltks98 <wltks98@hanmail.net>
2022-11-06 08:19:31 +0900

    [#104] style : 개행 추가

--- backend/src/main/java/com/huemap/backend/suggestion/application/SuggestionService.java
+++ backend/src/main/java/com/huemap/backend/suggestion/application/SuggestionService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.huemap.backend.suggestion.dto.request.SuggestionCreateRequest;
 import com.huemap.backend.suggestion.dto.response.SuggestionCreateResponse;
+
 @Service
 public class SuggestionService {
 	public SuggestionCreateResponse save(SuggestionCreateRequest suggestionCreateRequest) {
