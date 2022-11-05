cbcb0dd8d84570c432d9bccafd9a8468b7f89074
wltks98 <wltks98@hanmail.net>
2022-11-05 18:00:50 +0900

    [#104] feat : Suggestion Service 틀 구현

---
+++ backend/src/main/java/com/huemap/backend/suggestion/application/SuggestionService.java
@@ -0,0 +1,10 @@
+package com.huemap.backend.suggestion.application;
+
+import com.huemap.backend.suggestion.dto.request.SuggestionCreateRequest;
+import com.huemap.backend.suggestion.dto.response.SuggestionCreateResponse;
+
+public class SuggestionService {
+	public static SuggestionCreateResponse save(SuggestionCreateRequest suggestionCreateRequest) {
+		return null;
+	}
+}
