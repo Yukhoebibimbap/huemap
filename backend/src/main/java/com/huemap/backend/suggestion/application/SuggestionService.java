bea3c38320df5c2185301f2b2a595a3a41d5b613
wltks98 <wltks98@hanmail.net>
2022-11-05 22:56:41 +0900

    [#104] fix : @Service 추가

--- backend/src/main/java/com/huemap/backend/suggestion/application/SuggestionService.java
+++ backend/src/main/java/com/huemap/backend/suggestion/application/SuggestionService.java
@@ -1,8 +1,10 @@
 package com.huemap.backend.suggestion.application;
 
+import org.springframework.stereotype.Service;
+
 import com.huemap.backend.suggestion.dto.request.SuggestionCreateRequest;
 import com.huemap.backend.suggestion.dto.response.SuggestionCreateResponse;
-
+@Service
 public class SuggestionService {
 	public SuggestionCreateResponse save(SuggestionCreateRequest suggestionCreateRequest) {
 		return null;
