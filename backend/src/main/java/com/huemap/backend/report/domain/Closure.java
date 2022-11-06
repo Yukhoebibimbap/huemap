01d0784a91112dc12905f5a14c0a2a8f96249eda
ecvheo1 <ecvheo@naver.com>
2022-11-06 21:02:03 +0900

    [#93] feat: deleted 필드 추가

--- backend/src/main/java/com/huemap/backend/report/domain/Closure.java
+++ backend/src/main/java/com/huemap/backend/report/domain/Closure.java
@@ -10,20 +10,28 @@ import com.huemap.backend.bin.domain.Bin;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @DiscriminatorValue("CLOSURE")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Closure extends Report {
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "bin_id")
   private Bin bin;
 
+  private boolean deleted;
+
   @Builder
   public Closure(final Long userId, final Bin bin) {
     super(userId);
     this.bin = bin;
+  }
+
+  public void delete() {
+    this.deleted = true;
   }
 }
