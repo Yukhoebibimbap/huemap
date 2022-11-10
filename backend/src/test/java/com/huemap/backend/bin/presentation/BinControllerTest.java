a06df4470a1f8edc4523401eb0aaf4d036ee09ea
wltks98 <wltks98@hanmail.net>
2022-11-10 13:35:20 +0900

    [#130] test : 폐수거함 상세 조회 테스트 코드 작성

--- backend/src/test/java/com/huemap/backend/bin/presentation/BinControllerTest.java
+++ backend/src/test/java/com/huemap/backend/bin/presentation/BinControllerTest.java
@@ -1,5 +1,8 @@
 package com.huemap.backend.bin.presentation;
 
+import static com.huemap.backend.common.TestUtils.getBin;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -18,6 +21,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.huemap.backend.bin.application.BinService;
+import com.huemap.backend.bin.domain.Bin;
+import com.huemap.backend.bin.dto.response.BinDetailResponse;
+import com.huemap.backend.common.exception.EntityNotFoundException;
+import com.huemap.backend.common.response.error.ErrorCode;
 
 @WebMvcTest(BinController.class)
 @MockBean(JpaMetamodelMappingContext.class)
@@ -58,7 +65,7 @@ public class BinControllerTest {
 
 			@Test
 			@DisplayName("200을 응답한다.")
-			void It_throws_exception() throws Exception {
+			void It_responses_200() throws Exception {
 				//given
 				final String type = "GENERAL";
 
@@ -74,6 +81,57 @@ public class BinControllerTest {
 
 		private ResultActions requestFindAll(final String type) throws Exception {
 			return requestGet("/api/v1/bins?type=" + type);
+		}
+	}
+
+	@Nested
+	@DisplayName("findById 메소드는")
+	class findById {
+
+		@Nested
+		@DisplayName("폐수거함 id가 올바르면")
+		class Context_with_valid_id {
+
+			@Test
+			@DisplayName("200을 응답한다.")
+			void It_responses_200() throws Exception {
+				//given
+				final Long id = 1L;
+				final Bin bin = getBin();
+				given(binService.findById(id)).willReturn(BinDetailResponse.toDto(bin,anyBoolean()));
+
+				//when
+				final ResultActions perform = requestFindById(id);
+
+				//then
+				perform.andExpect(status().isOk())
+					.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$.data.id").value(1L))
+					.andExpect(jsonPath("$.data.address").value("서울특별시 종로구 창덕궁7길 5"));
+			}
+		}
+
+		@Nested
+		@DisplayName("존재하지 않는 폐수거함 id가 넘어오면")
+		class Context_with_invalid_id {
+
+			@Test
+			@DisplayName("예외를 던진다.")
+			void It_throws_exception() throws Exception {
+				//given
+				final Long id = 2L;
+				given(binService.findById(id)).willThrow(new EntityNotFoundException(ErrorCode.BIN_NOT_FOUND));
+
+				//when
+				final ResultActions perform = requestFindById(id);
+
+				//then
+				perform.andExpect(status().isBadRequest());
+			}
+		}
+
+		private ResultActions requestFindById(final Long id) throws Exception {
+			return requestGet("/api/v1/bins/" + id);
 		}
 	}
 
