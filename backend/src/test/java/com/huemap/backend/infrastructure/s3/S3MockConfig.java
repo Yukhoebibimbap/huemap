103758514add6212c6842a9295dcc5aa3996be5a
ecvheo1 <ecvheo@naver.com>
2022-11-12 13:12:12 +0900

    [#132] feat: s3 mock config 설정

---
+++ backend/src/test/java/com/huemap/backend/infrastructure/s3/S3MockConfig.java
@@ -0,0 +1,39 @@
+package com.huemap.backend.infrastructure.s3;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import io.findify.s3mock.S3Mock;
+
+@Profile("test")
+@TestConfiguration
+public class S3MockConfig {
+
+  @Bean(name = "s3Mock")
+  public S3Mock s3Mock() {
+    return new S3Mock.Builder().withPort(8001).withInMemoryBackend().build();
+  }
+
+  @Bean(name = "amazonS3")
+  @Primary
+  public AmazonS3 amazonS3(S3Mock s3Mock){
+    AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(
+        "http://localhost:8001", Regions.AP_NORTHEAST_2.name());
+    AmazonS3 client = AmazonS3ClientBuilder
+        .standard()
+        .withPathStyleAccessEnabled(true)
+        .withEndpointConfiguration(endpoint)
+        .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
+        .build();
+    return client;
+  }
+}
