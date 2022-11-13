dd3f88ca725397484c3c38cd7541fed824e0ec6f
ecvheo1 <ecvheo@naver.com>
2022-11-14 00:39:22 +0900

    [#152] chore: redis 설정 수정

--- backend/src/main/java/com/huemap/backend/common/config/RedisConfig.java
+++ backend/src/main/java/com/huemap/backend/common/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.huemap.backend.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,9 +9,15 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 @Configuration
 public class RedisConfig {
 
+  @Value("${spring.redis.host}")
+  private String host;
+
+  @Value("${spring.redis.port}")
+  private int port;
+
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
+    return new LettuceConnectionFactory(host, port);
   }
 
 }
