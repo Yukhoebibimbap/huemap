f54e9a1c93eaacca29d877718a4e0694b469769a
wltks98 <wltks98@hanmail.net>
2022-11-15 14:21:01 +0900

    [#160] feat : condition 엔티티에 conditionType 추가

--- backend/src/main/java/com/huemap/backend/domain/report/domain/Condition.java
+++ backend/src/main/java/com/huemap/backend/domain/report/domain/Condition.java
@@ -3,12 +3,15 @@ package com.huemap.backend.domain.report.domain;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.huemap.backend.domain.bin.domain.Bin;
+import com.huemap.backend.domain.bin.domain.ConditionType;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -29,12 +32,16 @@ public class Condition extends Report {
   @Embedded
   private Image image;
 
+  @Enumerated(EnumType.STRING)
+  private ConditionType type;
+
   private boolean deleted;
 
   @Builder
-  public Condition(final Long userId, final Bin bin, final Image image) {
+  public Condition(final Long userId, final Bin bin, final Image image, final ConditionType type) {
     super(userId);
     this.bin = bin;
     this.image = image;
+    this.type = type;
   }
 }
