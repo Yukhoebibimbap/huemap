144ffdada49d3a4956bdcc4d502ca5e7aa42cea2
ecvheo1 <ecvheo@naver.com>
2022-11-15 13:48:39 +0900

    [#152] feat: User 생성 Controller 구현

---
+++ backend/src/main/java/com/huemap/backend/domain/user/application/UserService.java
@@ -0,0 +1,10 @@
+package com.huemap.backend.domain.user.application;
+
+import com.huemap.backend.domain.user.dto.request.UserCreateRequest;
+
+public class UserService {
+
+  public Object save(UserCreateRequest userCreateRequest) {
+    return null;
+  }
+}
---
+++ backend/src/main/java/com/huemap/backend/domain/user/presentation/UserController.java
@@ -0,0 +1,32 @@
+package com.huemap.backend.domain.user.presentation;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.huemap.backend.common.response.success.RestResponse;
+import com.huemap.backend.domain.user.application.UserService;
+import com.huemap.backend.domain.user.dto.request.UserCreateRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+  private final UserService userService;
+
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping
+  public RestResponse saveClosure(
+      @RequestBody @Valid UserCreateRequest userCreateRequest
+  ) {
+    return RestResponse.of(userService.save(userCreateRequest));
+  }
+}
