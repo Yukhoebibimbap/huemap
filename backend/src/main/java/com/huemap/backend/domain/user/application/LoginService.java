36fac3829b3a2994629902d6ba8c811b56138eec
ecvheo1 <ecvheo@naver.com>
2022-11-16 08:21:46 +0900

    [#165] feat: 로그인 인터페이스 생성

---
+++ backend/src/main/java/com/huemap/backend/domain/user/application/LoginService.java
@@ -0,0 +1,9 @@
+package com.huemap.backend.domain.user.application;
+
+import com.huemap.backend.domain.user.dto.request.UserLoginRequest;
+import com.huemap.backend.domain.user.dto.response.UserLoginResponse;
+
+public interface LoginService {
+
+  UserLoginResponse login(UserLoginRequest userLoginRequest);
+}
