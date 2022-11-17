210001ab015ddbe1e1f7a6972c864f945586c6a3
ecvheo1 <ecvheo@naver.com>
2022-11-17 10:45:23 +0900

    [#168] feat: 인증 인터셉터 구현

---
+++ backend/src/main/java/com/huemap/backend/common/resolver/AuthInterceptor.java
@@ -0,0 +1,41 @@
+package com.huemap.backend.common.resolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.huemap.backend.common.annotation.AuthRequired;
+import com.huemap.backend.common.exception.AuthException;
+import com.huemap.backend.common.response.error.ErrorCode;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    if (!(handler instanceof HandlerMethod)) {
+      return true;
+    }
+
+    HandlerMethod handlerMethod = (HandlerMethod)handler;
+    AuthRequired authRequired = handlerMethod.getMethodAnnotation(AuthRequired.class);
+    if (authRequired == null) {
+      return true;
+    }
+
+    HttpSession httpSession = request.getSession();
+    Long userId = (Long)httpSession.getAttribute("USER_ID");
+    if (userId == null) {
+      throw new AuthException(ErrorCode.AUTH_ERROR);
+    }
+
+    return true;
+  }
+}
