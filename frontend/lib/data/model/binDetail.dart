b9767be5cbab6683f71bc96a57bc653698d10a57
Dongdong351 <dolong014@gmail.com>
2022-12-04 20:13:33 +0900

    feat: add bin detail object

---
+++ frontend/lib/data/model/binDetail.dart
@@ -0,0 +1,63 @@
+class BinDetail {
+  final int id;
+  final String gu;
+  final double lat, lng;
+  final String address;
+  final String addressDesc;
+  final String type;
+  final String updatedAt;
+  final bool hasClosure;
+  final bool isCandidate;
+
+  // json 생성자
+  BinDetail.fromJson(Map<String, dynamic> json) :
+        id = json['id'],
+        gu = json['gu'],
+        lat = json['latitude'],
+        lng = json['longitude'],
+        address = json['address'],
+        addressDesc = json['addressDescription'],
+        type = json['type'],
+        updatedAt = json['updatedAt'],
+        hasClosure = json['hasClosure'],
+        isCandidate = json['isCandidate'];
+}
+
+final Map detailString = {
+  'address': '도로명 주소',
+  'addressDesc': '상세 설명',
+  'type': '수거함 유형'
+};
+
+final Map binTypesKor = {
+  'GENERAL': '일반 쓰레기',
+  'RECYCLE': '재활용 쓰레기',
+  'LAMP': '폐형광등',
+  'BATTERY': '폐건전지',
+  'CLOTHES': '의류',
+  'MEDICINE': '폐의약품'
+};
+
+final Map binTypesEng = {
+  '일반 쓰레기': 'GENERAL',
+  '재활용 쓰레기': 'RECYCLE',
+  '폐형광등': 'LAMP',
+  '폐건전지': 'BATTERY',
+  '의류': 'CLOTHES',
+  '폐의약품': 'MEDICINE'
+};
+
+// enum Type  {
+//   general,
+//   recycle,
+//   lamp,
+//   battery,
+//   clothes,
+//   medicine
+// }
+//
+// extension ParseToString on Type {
+//   String toParameter() {
+//     return (this.toString().split('.').last).toUpperCase();
+//   }
+// }
