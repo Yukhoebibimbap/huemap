98221b6c1cc4a20f5861944544342b8e6a31d8d0
ecvheo1 <ecvheo@naver.com>
2022-12-06 21:42:26 +0900

    [#217] feat: jwt 설정 binding 클래스 생성

--- backend/src/main/java/com/huemap/backend/BackendApplication.java
+++ backend/src/main/java/com/huemap/backend/BackendApplication.java
@@ -2,9 +2,13 @@ package com.huemap.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.huemap.backend.common.config.JwtConfig;
+
+@EnableConfigurationProperties(JwtConfig.class)
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
