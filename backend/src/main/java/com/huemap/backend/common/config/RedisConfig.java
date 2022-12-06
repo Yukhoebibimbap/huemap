79f59494c2f0fc52eb6b4c4c76098bf8c595bc6f
ecvheo1 <ecvheo@naver.com>
2022-12-07 00:15:53 +0900

    [#217] feat: redis 설정

--- backend/src/main/java/com/huemap/backend/common/config/RedisConfig.java
+++ backend/src/main/java/com/huemap/backend/common/config/RedisConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
   @Value("${spring.redis.host}")
@@ -20,4 +24,12 @@ public class RedisConfig {
     return new LettuceConnectionFactory(host, port);
   }
 
+  @Bean
+  public RedisTemplate<String, String> redisTemplate() {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    return redisTemplate;
+  }
 }
